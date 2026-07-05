83c3af5d78aababd2b1c8d770e41a6cce3b8e3a2
Manash Protim Das <shatvayu@gmail.com>
2026-07-06 00:04:38 +0530

    bdd

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-app.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-app.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyDXt3A2bAA6goMKnOSQMbHsFuavTNQlOf4",
+  authDomain: "tanman-fasciamax.firebaseapp.com",
+  projectId: "tanman-fasciamax",
+  storageBucket: "tanman-fasciamax.firebasestorage.app",
+  messagingSenderId: "504111897476",
+  appId: "1:504111897476:web:ff0ae3a9a95567af1c5bd1"
 };
 
 const app = initializeApp(firebaseConfig);
